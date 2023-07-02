654d2d9158370c613e8a5e7618c2ea140eadb463
Sapna <kumawatsapna199@gmail.com>
2023-07-03 01:19:03 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Card } from "./components/Card";
+import {getAreaFoodItems, getAreaList} from './api/services/food';
 
 function App() {
+  const [foodItems, setFoodItems] = useState([]);
+  const [areas, setAreas] = useState([]);
+
+  const fetchAreaList = async () => {
+    const response = await getAreaList();
+    if(response.data.meals && response.data.meals.length > 0){
+      setAreas(response.data.meals);
+      fetchFoodItems(response.data.meals[0].strArea)
+    }
+  }
+
+  const fetchFoodItems = async (area) => {
+    const response = await getAreaFoodItems(area);
+    setFoodItems(response.data.meals);
+  }
+
+  const handleAreaChange = (e) => fetchFoodItems(e.target.value);
+
+  useEffect(() => {
+    fetchAreaList();
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <section className="container">
+    <div className="p-2 px-4 d-flex align-items-center justify-content-between bg-light rounded border border-muted">
+      <select onChange={handleAreaChange} className="p-2 px-3 border border-muted">
+        {
+          areas.map(area => (
+            <option key={area.strArea} value={area.strArea}>
+              {area.strArea}
+            </option>
+          ))
+        }
+      </select>
+      <div className="bg-dark p-3 text-white rounded">
+        {foodItems.length}
+      </div>
     </div>
+
+    <div className="row my-4">
+      {
+        foodItems.map(food => (
+          <div className="col-12 p-3 col-md-4 col-lg-3">
+            <Card image={food.strMealThumb} title={food.strMeal}/>
+          </div>
+        ))
+      }
+    </div>
+   </section>
   );
 }
 
